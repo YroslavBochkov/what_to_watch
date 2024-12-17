0223b14fa75163909d37ff9ade6b3fc593c73627
Yaroslav Bochkov <cruelty86@yandex.ru>
2024-12-17 13:47:21 +0300

    update get func

---
+++ opinions_app/api_views.py
@@ -0,0 +1,13 @@
+# what_to_watch/opinions_app/api_views.py
+
+from flask import jsonify
+
+from . import app
+from .models import Opinion
+
+
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
+def get_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    # Никаких лишних функций, просто метод to_dict():
+    return jsonify({'opinion': opinion.to_dict()}), 200
